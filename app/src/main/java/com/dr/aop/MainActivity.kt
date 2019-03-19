4b70470a522bed11a4c5c4273e418933eca1851b
tank2014gz <tank2014guangzhou@qq.com>
2019-03-19 14:59:24 +0800

    first commit

---
+++ app/src/main/java/com/dr/aop/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.dr.aop
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
